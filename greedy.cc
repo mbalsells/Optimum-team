fd623a9801a0d4bf5a1963cdb23708100baf9b0c
mbalsells <43616135+mbalsells@users.noreply.github.com>
2019-12-10 22:43:16 +0100

    compila i funciona

--- greedy.cc
+++ greedy.cc
@@ -8,8 +8,6 @@
 
 using namespace std;
 
-const int INF = 2e9;
-
 struct player {
     string name, position, club;
     int price, points;          
@@ -18,23 +16,19 @@ struct player {
 string output_file; //name of the output file.
 clock_t initial_time; //time when the program started.
 
-vector <vector <player> > classified_players(4);
-// Classified_players[position] has all the players that have that position,
-// considering that the position ranges from 0 to 3 where 0 is goalkeeper,
-// 1 is defense, 2 is midfielder and 3 is forward.
-
 vector <int> formation; 
 // Number of required players for each position.
 
 bool comp (const player& p1, const player& p2) {
-	if (p1.points != p2.points) return p1.points < p2.points;
+    if (p1.points != p2.points) return p1.points > p2.points;
     return p1.price < p2.price;
 }
 
 bool order_position (const player& p1, const player& p2) {
     map <string, int> classifier = {{"por", 0}, {"def", 1}, {"mig", 2}, {"dav", 3}};
 
-    if (classifier[p1] != classifier[p2]) return classifier[p1] < classifier[p2];
+    if (classifier[p1.position] != classifier[p2.position]) 
+        return classifier[p1.position] < classifier[p2.position];
     if (p1.points != p2.points) return p1.points > p2.points;
     if (p1.price != p2.price) return p1.price < p2.price;
     return p1.name < p2.name;
@@ -42,8 +36,9 @@ bool order_position (const player& p1, const player& p2) {
 
 // Given the document in which the output will be written and a
 // team, writes the team in the document with the required format.
-void print_solution(vector <int>& team){
-    sort(team.begin(), sort.end(), order_position());
+void print_solution(vector <player>& team){
+
+    sort(team.begin(), team.end(), order_position);
     vector <string> encode = {"POR", "DEF", "MIG", "DAV"};
     
     int points = 0, price = 0; //Total points and price of the team.
@@ -81,12 +76,11 @@ void print_solution(vector <int>& team){
 // This function is used to initiallize the variables used in the program.
 // It is the case of the formation vector, the classified_players matrix,
 // and it fixes the states of the memoization vector.
-void initialize(vector <player>& players, int N1, int N2, int N3, int max_price){
-    formation = {1, N1, N2, N3};
+void initialize(vector <player>& players, int max_price){
     vector <player> aux;
     for (player p : players) if (p.price <= max_price) aux.push_back(p);
     players = aux;
-    sort(players.begin(), players.end(), comp()); // Sort the vector of players.
+    sort(players.begin(), players.end(), comp); // Sort the vector of players.
 }
 
 
@@ -100,14 +94,14 @@ void greedy(vector <player>& players, int budget){
     vector <player> ans;
     for (int i = 0; i < players.size(); ++i){
         player current = players[i];
-        int position = classifier[current.pos];
+        int position = classifier[current.position];
 
-        if (budget >= current.price) continue;
-        if (formed[position] == formation[position]) return;
+        if (budget < current.price) continue;
+        if (formed[position] == formation[position]) continue;
             
         budget -= current.price;
         ++formed[position];
-        ans.push_back(ans);
+        ans.push_back(current);
     }
 
     print_solution(ans);
@@ -117,14 +111,39 @@ void greedy(vector <player>& players, int budget){
 // returns the optimum team, i.e. the one that satisfies all the
 // constraints and has the highest total score. Also, out of all the ones
 // that share such a score, we break ties by taking the cheapest one.
-void get_solution(string input_file, const vector <player>& players){
+void get_solution(string input_file, vector <player>& players){
     ifstream in(input_file);
     int N1, N2, N3, max_price_team, max_price_player;
     in >> N1 >> N2 >> N3 >> max_price_team >> max_price_player;
     in.close();
 
-    initialize(players, N1, N2, N3, max_price_player); //Initialize variables.
-    greedy(max_price_team); //Solve the problem.
+    formation = {1, N1, N2, N3};
+     
+    initialize(players, max_price_player); //Initialize variables.
+    greedy(players, max_price_team); //Solve the problem.
+}
+
+// Given the file with the data of the football players, the function returns
+// all the players in the database.
+vector <player> read_data(string input_file){
+    vector <player> players;
+
+    ifstream in(input_file);
+    while (not in.eof()) {
+        string nom, posicio, club;
+        int punts, preu;
+        getline(in,nom,';');        if (nom == "") break;
+        getline(in,posicio,';');
+        in >> preu;
+        char aux; in >> aux;
+        getline(in,club,';');
+        in >> punts;
+        string aux2;
+        getline(in,aux2);
+        players.push_back({nom, posicio, club, preu, punts});
+    }  
+
+    return players;
 }
 
 int main(int argc, char** argv) {
@@ -137,7 +156,6 @@ int main(int argc, char** argv) {
     }
 
     output_file = argv[3];
-
     vector <player> players = read_data(argv[1]);
     get_solution(argv[2], players); 
 }
